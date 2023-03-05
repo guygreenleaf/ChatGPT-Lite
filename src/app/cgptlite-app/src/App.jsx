765c5b21d3b541a13e486a70db5f577f7be62243
guygreenleaf <guygreenleaf@icloud.com>
2023-03-05 14:47:20 -0800

    working app, updated readme

--- src/app/cgptlite-app/src/App.jsx
+++ src/app/cgptlite-app/src/App.jsx
@@ -1,20 +1,46 @@
 import { useState } from 'react';
+import OpenAIContext from './OpenAIContext/OpenAIContext';
+
 
 function App() {
   const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  let submitPrompt = () =>{
-    console.log(prompt);
+  let aiContext = new OpenAIContext();
+
+  let submitPrompt = async() =>{
+    try{
+      setIsLoading(true);
+      let res = await aiContext.SubmitPrompt(prompt);
+      setResponse(res);
+
+    } catch(e){
+      console.log(e);
+      setResponse("Error retrieving response.");
+
+    } finally{
+      setIsLoading(false);
+    }
   };
 
   return (
     <div>
       <h1>ChatGPT Lite</h1>
-      <div>
-        <textarea value={prompt} onChange={(e) => setPrompt(e.target.value)} />
-        <button onClick={submitPrompt}>
-          Submit Prompt
-        </button>
+      <div style={{display:'flex', flexDirection:'column',  alignItems:'center'}}>
+        <div>        
+          <textarea style={{height:'20vh'}} value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+        </div>
+        <div>        
+          <button onClick={submitPrompt}>
+              Submit Prompt
+            </button>
+        </div>
+      </div>
+
+      <div style={{display:'flex', flexDirection:'column',  alignItems:'center'}}>
+        <h4>Response:</h4>
+        {isLoading ? (<span>Loading...</span>) : (<span>{response}</span>)}
       </div>
     </div>
   );
---
+++ src/app/cgptlite-app/src/OpenAIContext/OpenAIContext.js
@@ -0,0 +1,23 @@
+import { Configuration, OpenAIApi } from "openai";
+
+export default class OpenAIContext{
+    constructor(){
+        this.apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+        this.openAIContext = new OpenAIApi(new Configuration({
+            apiKey: import.meta.env.VITE_OPENAI_API_KEY
+          }));
+    }
+
+    async SubmitPrompt(prompt){
+        const response = await this.openAIContext.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{role:"user", content:prompt}] ,
+          temperature: 0.7,
+          max_tokens: 256,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+      });
+      return response.data.choices[0].message.content;
+    }
+}
